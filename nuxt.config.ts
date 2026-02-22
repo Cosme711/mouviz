9ecb1d234d00763337a710bb07059b4ac236e7e5
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2026-02-22 21:58:25 +0100

    chore: init shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  css: ['~/assets/css/tailwind.css'],
+
+  modules: ['shadcn-nuxt'],
+
+  shadcn: {
+    prefix: '',
+    componentDir: '~/components/ui',
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
